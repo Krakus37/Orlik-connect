1d421d0cf2741d6e2d081849b225720a7ca03909
Krakus37 <szkrakus@op.pl>
2025-06-08 09:51:50 +0200

    Delete index(1).js

--- index(1).js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main style={{ padding: 20, fontFamily: 'Arial, sans-serif' }}>
-      <h1>OrlikConnect</h1>
-      <p>Twoja aplikacja do umawiania się na mecze w Skierniewicach</p>
-    </main>
-  );
-}
